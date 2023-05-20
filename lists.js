f620ccdc6bccb6518edda5047c78923a201de755
CypressKids11 <134014028+CypressKids11@users.noreply.github.com>
2023-05-20 12:51:24 -0500

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,13 @@
+export let weaponList = [
+    "./images/mallet.png",    
+    "./images/slingshot.png",
+    "./images/bow-arrow.png",
+    "./images/flamethrower.png",
+    "./images/watergun.png",
+    "./images/laser-pistol.png",
+    "./images/laser-blaster.png",
+    "./images/laser-sniper-rifle.png",
+    "./images/rpg.png",
+    "./images/poison-gas-cannon.png",
+    "./images/atom-bomb.png",
+];
